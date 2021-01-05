e495412c2f36f36568da1c12afdb15238365fb73
Denis Turbal <Denis.Turbal@softeq.com>
2021-01-05 11:58:34 +0300

    Initial commit

---
+++ src/main/java/com/turbal/cnb/CnbApplication.java
@@ -0,0 +1,13 @@
+package com.turbal.cnb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CnbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CnbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/turbal/cnb/CnbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turbal.cnb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CnbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
