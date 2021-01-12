e0b15bc7de01f14ed894574be72092a4d3084fa7
Denis Turbal <Denis.Turbal@softeq.com>
2021-01-12 18:18:44 +0300

    Added migration for adding auditable columns

---
+++ src/main/resources/db/migration/V3__Add_Auditable.sql
@@ -0,0 +1,28 @@
+/*
+ * Developed by Turbal Denis
+ * https://github.com/EnterCapchaCode
+ */
+
+alter table comment add column version bigint default 0;
+alter table comment add column created timestamp not null default now();
+alter table comment add column modified timestamp;
+
+alter table employee add column version bigint default 0;
+alter table employee add column created timestamp not null default now();
+alter table employee add column modified timestamp;
+
+alter table post add column version bigint default 0;
+alter table post add column created timestamp not null default now();
+alter table post add column modified timestamp;
+
+alter table post_tag add column version bigint default 0;
+alter table post_tag add column created timestamp not null default now();
+alter table post_tag add column modified timestamp;
+
+alter table role add column version bigint default 0;
+alter table role add column created timestamp not null default now();
+alter table role add column modified timestamp;
+
+alter table tag add column version bigint default 0;
+alter table tag add column created timestamp not null default now();
+alter table tag add column modified timestamp;
