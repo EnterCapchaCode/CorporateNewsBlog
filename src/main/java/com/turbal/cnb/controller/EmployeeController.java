b269ce66cd38bc80f5774abbabe673dc8bb4c149
Denis Turbal <Denis.Turbal@softeq.com>
2021-01-13 14:49:29 +0300

    Added endpoints for Employee

--- src/main/java/com/turbal/cnb/controller/EmployeeController.java
+++ src/main/java/com/turbal/cnb/controller/EmployeeController.java
@@ -10,10 +10,16 @@ import com.turbal.cnb.exception.ValidationException;
 import com.turbal.cnb.service.EmployeeService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/employees")
@@ -25,7 +31,22 @@ public class EmployeeController {
 
     @PostMapping
     public EmployeeDto saveEmployee(@RequestBody EmployeeDto employeeDto) throws ValidationException {
-        log.info("Handling save employee: " + employeeDto);
         return employeeService.saveEmployee(employeeDto);
+    }
+
+    @GetMapping
+    public List<EmployeeDto> findAllEmployees() {
+        return employeeService.findAll();
+    }
+
+    @GetMapping("{login}")
+    private EmployeeDto findByLogin(@PathVariable String login) {
+        return employeeService.findByLogin(login);
+    }
+
+    @DeleteMapping("{id}")
+    private ResponseEntity<Void> deleteEmployee(@PathVariable Integer id) {
+        employeeService.deleteEmployee(id);
+        return ResponseEntity.ok().build();
     }
 }
